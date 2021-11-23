589760235b2e54368cbab944e6ebbad6e03d7dab
Philippe <56139194+philippe8910@users.noreply.github.com>
2021-11-24 00:55:20 +0800

    Hack均分

--- Assets/NetworkCallBack.cs
+++ Assets/NetworkCallBack.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using Photon.Bolt;
+using Photon.Bolt.Matchmaking;
+using UdpKit;
+using UdpKit.Platform.Photon;
 using UnityEngine;
 
 public class NetworkCallBack : GlobalEventListener
 {
     [SerializeField] private GameObject Hack , Bouble;
+
+    [SerializeField] private int x = 0;
     
     public override void SceneLoadLocalDone(string scene, IProtocolToken token)
     {
+        Debug.Log(BoltMatchmaking.CurrentSession.ConnectionsCurrent);
         BoltNetwork.Instantiate(Hack , Vector3.zero,  Quaternion.identity);
     }
 }

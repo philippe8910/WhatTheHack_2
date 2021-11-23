a506d5545891db6c4ae49e68d24512a1313fc6b4
toast0129 <j27272589@gmail.com>
2021-11-24 01:56:17 +0800

    Menu_3(ExitButton,CharacterChoose)

--- Assets/Script/MenuMousePosition.cs
+++ Assets/Script/MenuMousePosition.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class MenuMousePosition : MonoBehaviour
 {
-    Vector2 mousepos = Camera.main.ScreenToViewportPoint( Input.mousePosition );
-
+    [SerializeField] public Vector2 MousePos;
+    [SerializeField] public GameObject HackChoose;
+    [SerializeField] public GameObject BubbleChoose;
+    [SerializeField] public GameObject Bubble;
+    [SerializeField] public GameObject Hack;
+   
     void Start()
     {
         
@@ -13,7 +17,32 @@ public class MenuMousePosition : MonoBehaviour
     
     void Update()
     {
-      Debug.Log(mousepos);
+        mouseposcheck();
+    }
+
+    bool mouseisleft()
+    {
+        return (MousePos.x < 0.5f);
+    }
+    public void mouseposcheck()
+    {
+        Vector2 mousepos = Camera.main.ScreenToViewportPoint( Input.mousePosition );
+        MousePos = mousepos;
+        if (mouseisleft() == true)
+        {
+            Bubble.SetActive(true);
+            Hack.SetActive(false);
+            HackChoose.SetActive(true);
+            BubbleChoose.SetActive(false);
+        }
+        else
+        {
+            Bubble.SetActive(false);
+            Hack.SetActive(true);
+            HackChoose.SetActive(false);
+            BubbleChoose.SetActive(true);
+        }
+        
     }
 
 }
